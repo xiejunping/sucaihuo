4b646698b7828d0173533d0be04f23d27eebee83
Cobbler <417663733@qq.com>
2018-06-14 20:02:08 +0800

    经常报错，现已启动队列模式

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const cheerio = require('cheerio');
 const model = require('./http/api');
 
 const users = require('./config/user');
+const Queue = require('promise-queue-plus')
+const queue = new Queue(1, {
+    'retry': 3,
+    'retryIsJump': false,
+    'timeout': 2000
+})
 
 const showColumn = async (user, pwd) => {
     const data = await model.userLogin({
@@ -17,18 +23,30 @@ const showColumn = async (user, pwd) => {
 
     const data2 = await model.signDay(key, data.cookie);
 
-    return `${key}: ${data2}`;
+    console.log(`${key}-${user}: ${data2}`);
+    // return `${key}: ${data2}`;
 };
 
 const forShowInfo = async () => {
     console.time('forShowInfo');
 
-    const promises = users.map((x) => showColumn(x.name, x.pass));
-    for (const promise of promises) {
-        const info = await promise;
-
-        console.log(`INFO: ${info}`);
+    for(const x of users) {
+        if (queue.isStart()) {
+            queue.push(showColumn, [x.name, x.pass])
+        } else {
+            // 队列未启动，启动
+            queue.go(showColumn, [x.name, x.pass]).then(() => {
+                console.log(`队列已启动`)
+            })
+        }
     }
+
+    // const promises = users.map((x) => showColumn(x.name, x.pass));
+    // for (const promise of promises) {
+    //     const info = await promise;
+    //
+    //     console.log(`INFO: ${info}`);
+    // }
 
     console.timeEnd('forShowInfo');
 };
