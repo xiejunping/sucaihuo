94f80caf332ba95fba0c917710f1ac52dfbb0cae
Cobbler <417663733@qq.com>
2019-03-29 17:40:14 +0800

    null err

--- src/mongod/helper.js
+++ src/mongod/helper.js
@@ -14,25 +14,28 @@ class MongoDB {
         this.client = null; // 多次连接共享
     }
 
-    connect () {
+    connect (url, dbName) {
         return new Promise((resolve, reject) => {
             if (this.client) resolve(this.client);
-            const client = new MongoClient(this.url, { useNewUrlParser: true });
+            const client = new MongoClient(url, { useNewUrlParser: true });
             client.connect((err, pool) => {
-                err && reject(err);
-                console.log('Connected Mongodb to Server');
-                this.client = pool.db(this.database);
-                resolve(this.client);
+                console.log(err, pool)
+                if (err) reject(err);
+                else {
+                    console.log('Connected Mongodb to Server');
+                    this.client = pool.db(dbName);
+                    resolve(this.client);
+                }
             });
         })
     }
 
     insert (tableName, rowInfo) {
         return new Promise((resolve, reject) => {
-            this.connect().then(db => {
+            this.connect(this.url, this.database).then(db => {
                 db.collection(tableName).insertMany(rowInfo, (err, rs) => {
-                    err && reject(err);
-                    resolve(rs);
+                    if (err) reject(err);
+                    else resolve(rs);
                 });
             }).catch(err => {
                 console.log(err);
