b63036692b0e203e19896543b99de2ac76e77f9d
Cobbler <417663733@qq.com>
2020-11-04 17:45:11 +0800

    改邮箱密码

--- src/mail/config.js
+++ src/mail/config.js
@@ -1,8 +1,8 @@
 const config = {
     SERVICE: 'QQex',
     AUTH_ACCOUNT: 'noreply@jsvue.cn',
-    AUTH_PASS: 'CJdhuSiAEzWXBgmh',
-  
+    AUTH_PASS: 'RHmLuFyrHTEDGewh',
+
     FROM: '"VUE技术栈" <noreply@jsvue.cn>'
 };
 
